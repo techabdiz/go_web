85e141e77d4ac22f34292ee3ff4e21724d0ce889
techabdiz <tech.abdiz@gmail.com>
2025-01-19 00:49:02 +0530

    semaphores

--- c_files/threads/base.c
+++ c_files/threads/base.c
@@ -1,20 +1,51 @@
 #include <stdio.h>
 #include <pthread.h>
+#include <stdlib.h>
+#include <semaphore.h>
+
 
 void *thread(void *vargp);
 
-int main ( ) { 
-    pthread_t pt;
-    pthread_create(&pt, NULL, thread, "1");
-    pthread_create(&pt, NULL, thread, "2");
-    pthread_create(&pt, NULL, thread, "3");
-    pthread_create(&pt, NULL, thread, "4");
-    pthread_join(pt, NULL);
-    printf("main exit... !\n");
+volatile long cnt = 0;
+sem_t mutex;
+
+int main () { 
+    sem_init(&mutex, 0, 2);
+    int niters = 100000;
+    int num_threads = 2; 
+    pthread_t arr[num_threads];
+    for( int i = 0; i < num_threads; i ++ ) {
+        pthread_t t;
+        pthread_create(&t, NULL, thread, &niters);
+        arr[i] = t;
+    }  
+
+    for( int i = 0; i < num_threads; i ++ ) {
+            pthread_join(arr[i], NULL);
+    }
+
+    /*pthread_t t1;
+    pthread_t t2;
+    pthread_t t3;
+    pthread_t t4;
+    pthread_create(&t1, NULL, thread, &niters);
+    pthread_create(&t2, NULL, thread, &niters);
+    pthread_create(&t3, NULL, thread, &niters);
+    pthread_create(&t4, NULL, thread, &niters);
+    pthread_join(t1, NULL);
+    pthread_join(t2, NULL);
+    pthread_join(t3, NULL);
+    pthread_join(t4, NULL);*/
+    printf("return val: %d\n", cnt);
     return 0;
 }
 
-void* thread(void *vargp) { 
-    printf("Hello from thread %s\n", vargp);
+void* thread(void *vargp) {
+    
+    for (int i = 0; i < *(int*)vargp; i++) { 
+        sem_wait(&mutex);
+        cnt++;
+        sem_post(&mutex);
+    }
     return NULL;
 }
